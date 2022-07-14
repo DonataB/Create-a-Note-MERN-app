709805351b945fa6a2db2affa0e7aac2ef0c50d8
Your Name <donata1989@gmail.com>
2022-07-14 14:13:53 +0200

    Remove unnecessary import

--- client/src/components/views/HomePage/HomePage.tsx
+++ client/src/components/views/HomePage/HomePage.tsx
@@ -1,22 +1,17 @@
-import React from "react";
 import { Card, HomePageWrapper, Title, Text } from "./HomePage.style";
 
 const HomePage = () => {
-    return (
-      <HomePageWrapper>
-        <Card>
-  
-          {/* eslint-disable react/no-unescaped-entities */}
-          <Title>Let's create some notes!</Title>
-          <Text>
-            Every day, all over the world, everyone - from young children to
-            adults - takes notes...
-          </Text>
+	return (
+		<HomePageWrapper>
+			<Card>
+				<Title>Let's create some notes!</Title>
+				<Text>
+					Every day, all over the world, everyone - from young children to
+					adults - takes notes...
+				</Text>
+			</Card>
+		</HomePageWrapper>
+	);
+};
 
-        </Card>
-      </HomePageWrapper>
-    );
-  };
-  
-  export default HomePage;
-  
+export default HomePage;
