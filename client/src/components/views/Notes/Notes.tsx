179b434c054cd7caadf2f0873f245979f8bdaa80
Your Name <donata1989@gmail.com>
2022-08-03 14:15:00 +0200

    Remove unnecessary code

--- client/src/components/views/Notes/Notes.tsx
+++ client/src/components/views/Notes/Notes.tsx
@@ -1,35 +1,37 @@
 import React, { useEffect, useState } from "react";
 import Note from "./note.interface";
 import { GridContainer } from "../../wrappers/GridContainer.style";
-import { NoteCard, NoteTitle, NoteContent, Info, ButtonArea, BinIco, UpdateIco } from "./Notes.style";
-import { useParams } from "react-router-dom";
+import {
+	NoteCard,
+	NoteTitle,
+	NoteContent,
+	Info,
+	ButtonArea,
+	BinIco,
+	UpdateIco,
+} from "./Notes.style";
+// import { useParams } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { Toast } from "../../wrappers/Toast.style";
 
 interface INotes {
 	notes?: Note[];
 }
 
 export const Notes: React.FC<INotes> = ({ notes }) => {
-	const [not, setNot] :any = useState([]);
+	const [not, setNot]: any = useState([]);
 	// const { id } = useParams();
 	const handleDeleteNote = async (id: any) => {
 		try {
-			const data = await axios.delete(`http://localhost:3000/notes/${id}`);
-			console.log('Note successfully deleted');
+			await axios.delete(`http://localhost:3000/notes/${id}`);
+			console.log("Note successfully deleted");
 			window.location.reload();
 			return toast.success("Note deleted!");
 		} catch (error) {
 			return toast.error("Upps! Note wasn't deleted");
-		};
+		}
 	};
-
-	const handleRemoveNote = async (id: any) =>{
-		await handleDeleteNote(id)
-		const noteCopy = [...not]
-		noteCopy.filter(note => note._id !== id);
-		setNot(noteCopy);
-	}
 
 	return (
 		<GridContainer>
@@ -38,20 +40,17 @@ export const Notes: React.FC<INotes> = ({ notes }) => {
 					<NoteCard key={note._id}>
 						<NoteTitle>{note.title}</NoteTitle>
 						<NoteContent>{note.content}</NoteContent>
-						<ButtonArea >
-							<UpdateIco>
-							<Info>update</Info>
-							</UpdateIco>
-							<BinIco type='submit' onClick={() => handleRemoveNote(note._id)}>
-
-							<Info>delete</Info>
+						<ButtonArea>
+							{/* <UpdateIco>
+								<Info>update</Info>
+							</UpdateIco> */}
+							<BinIco type='submit' onClick={() => handleDeleteNote(note._id)}>
+								<Info>delete</Info>
 							</BinIco>
 						</ButtonArea>
-						{/* <DeleteBtn type='submit' onClick={() => handleRemoveNote(note._id)}>
-						</DeleteBtn> */}
+						<Toast />
 					</NoteCard>
 				))}
 		</GridContainer>
 	);
 };
-
