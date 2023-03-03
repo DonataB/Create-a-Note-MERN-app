bff6db2c5336ff5b575a132fef48419cbce97bb4
Your Name <donata1989@gmail.com>
2023-03-03 16:24:22 +0100

    update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cors(corsOptions));
 app.use("/notes", noteRoute);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/buil", "index.html"));
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 // app.get("*", (req, res) => {
 //   res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
