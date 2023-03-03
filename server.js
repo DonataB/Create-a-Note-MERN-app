7abe0391b5b89c36c83b67e9d1f689e6d996ce36
Your Name <donata1989@gmail.com>
2023-03-03 19:48:46 +0100

    update

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/notes", noteRoute);
 
 // });
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(process.env.PORT || 3000, () => {
